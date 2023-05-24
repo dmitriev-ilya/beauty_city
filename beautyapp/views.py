d2646763caacbfb82853681c5c37c421127901f4
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-05-24 21:55:00 +0500

    salons added on main page

--- beautyapp/views.py
+++ beautyapp/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
+from .models import Saloon
+
 
 def index(request):
-    return render(request, 'index.html')
+    salons = Saloon.objects.all()
+
+    salon_details = []
+    for salon in salons:
+        salon_detail = {
+            'name': salon.name,
+            'city': salon.city,
+            'address': salon.address,
+            'image': salon.avatar.url if salon.avatar else None
+        }
+        salon_details.append(salon_detail)
+
+    context = {
+        'salons': salon_details
+    }
+
+    return render(request, 'index.html', context=context)
 
 
 def notes(request):
