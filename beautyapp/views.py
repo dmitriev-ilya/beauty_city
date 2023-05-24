0a3e4b31e30f0111ee15a6d773b70903f78aeb2c
Vladuslav Dragushin <maxhc@inbox.ru>
2023-05-24 13:45:33 +0300

    Added popupjs script

--- beautyapp/views.py
+++ beautyapp/views.py
@@ -1,7 +1,18 @@
+import json
+
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def index(request):
+    if request.method == 'POST':
+        if request.POST:
+            print(1)
+        if not request.POST:
+            body_data = json.loads(request.body)
+            print(body_data)
+
     return render(request, 'index.html')
 
 
