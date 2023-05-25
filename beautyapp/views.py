734a7fecedb1727c3d12eef858422940b4550c58
Vladuslav Dragushin <maxhc@inbox.ru>
2023-05-25 16:38:36 +0300

    Added sms auth

--- beautyapp/views.py
+++ beautyapp/views.py
@@ -1,9 +1,15 @@
 import json
 import random
 
+from .send_sms import send_auth_sms
+from beauty_city.settings import SMS_KEY
+
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 from .models import User
+from django.contrib.auth.hashers import make_password
 
 
 @csrf_exempt
@@ -13,20 +19,31 @@ def index(request):
             print(1)
         if not request.POST:
             body_data = json.loads(request.body)
-            print(body_data)
             if 'tel' in body_data and 'code' not in body_data:
-                code = random.randint(1000, 9999)
+                code = str(random.randint(1000, 9999))
+
+                # Включать эту функцию только при тестах с Тимуром (смс платные)
+                # send_auth_sms(SMS_KEY, body_data['tel'], code)
                 User.objects.update_or_create(
                     username=body_data['tel'],
                     defaults={
                         'phone_number': body_data['tel'],
-                        'code': code
+                        'password': make_password(code),
+                        'code': code,
                     }
                 )
             if ('tel' and 'code') in body_data:
-                user = User.objects.get(username=body_data['tel'])
-                if str(user.code) == body_data['code']:
-                    print('Succes!')
+                user = authenticate(
+                    request,
+                    username=body_data['tel'],
+                    password=body_data['code']
+                )
+                print(user)
+                login(request, user)
+                if user:
+                    return JsonResponse({'success': True}, status=200)
+                else:
+                    return JsonResponse({'success': False}, status=403)
 
     return render(request, 'index.html')
 
